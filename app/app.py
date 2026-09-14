2615688689544bbcb76941f5ac10445c92036cf8
Sidqixx <sidqi.rahmad2703@gmail.com>
2026-09-14 17:58:43 +0700

    Add Streamlit secrets validation V.2

--- app/app.py
+++ app/app.py
@@ -10,8 +10,9 @@ import pandas as pd
 import streamlit as st
 from dotenv import load_dotenv
 
-st.write("SMTP_USERNAME:", os.getenv("SMTP_USERNAME"))
 st.write("SMTP_HOST:", os.getenv("SMTP_HOST"))
+st.write("SMTP_USERNAME exists:", bool(os.getenv("SMTP_USERNAME")))
+st.write("SMTP_USERNAME:", os.getenv("SMTP_USERNAME"))
 st.write("ENABLE_EMAIL:", os.getenv("ENABLE_EMAIL"))
 
 
